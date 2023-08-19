0169ba6b249d60b9624af3ec82bbfb63c9f92200
ZhangMin1998 <1464253423@qq.com>
2023-08-20 01:45:09 +0800

    多入口按需加载

--- demo_code_split/src/main.js
+++ demo_code_split/src/main.js
@@ -1,4 +1,15 @@
 import { sum } from "./math"
+import count from "./count"
 
 console.log("hello main")
 console.log(sum(1, 2, 3, 4, 5))
+
+document.getElementById('btn').onclick = function() {
+  // console.log(count(2,1))
+  // import动态导入 会将动态导入的文件分割，在需要时自动加载
+  import('./count').then((res) => {
+    console.log('加载ok', res)
+  }).catch(err => {
+    console.log(err)
+  })
+}
